7d591594abf25d275e0d35ace41823e8e5dc9a1d
9oya <eido9oya@gmail.com>
2019-10-18 08:12:33 +0900

    Dev layout

--- dymm_app_web/static/js/home.js
+++ dymm_app_web/static/js/home.js
@@ -1,8 +1,18 @@
 $(document).ready(function () {
 
-    // !(function () {
-    //     $.get("http://ip-api.com/json", function (response) {
-    //         console.log(response);
-    //     }, "jsonp");
-    // })();
+    !(function () {
+        $("#bx-home-first").show(400, function () {
+            $("#img-screen-1").show("slow", function () {
+                $("#img-screen-2").show("slow", function () {
+                    $("#img-screen-3").show("slow", function () {
+                        $("#bx-home-second").show("slow", function () {
+                            $("#bx-home-second").show("slow");
+                            $("#bx-home-third").show("slow");
+                            $("#footer").show("slow")
+                        })
+                    });
+                });
+            });
+        })
+    })();
 });
