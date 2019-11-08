50b5973b58eb9acde344dab5bf63673955fd9aed
9oya <eido9oya@gmail.com>
2019-11-08 15:01:58 +0900

    Full change

--- dymm_app_web/static/js/home.js
+++ dymm_app_web/static/js/home.js
@@ -1,18 +1,39 @@
 $(document).ready(function () {
+    AOS.init();
+    let owl = $('.owl-carousel');
 
-    !(function () {
-        $("#bx-home-first").show(400, function () {
-            $("#img-screen-1").show("slow", function () {
-                $("#img-screen-2").show("slow", function () {
-                    $("#img-screen-3").show("slow", function () {
-                        $("#bx-home-second").show("slow", function () {
-                            $("#bx-home-second").show("slow");
-                            $("#bx-home-third").show("slow");
-                            $("#footer").show("slow")
-                        })
-                    });
-                });
-            });
-        })
-    })();
+    owl.owlCarousel({
+        loop: false,
+        margin: 10,
+        nav:true,
+        video: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 3,
+                nav: false
+            },
+            1000: {
+                items: 5,
+                nav: true,
+                loop: false
+            }
+        }
+    });
+
+    // var prevScrollpos = window.pageYOffset;
+    // window.onscroll = function () {
+    //     var currentScrollPos = window.pageYOffset;
+    //     if (prevScrollpos > currentScrollPos) {
+    //         document.getElementById("navbar").style.top = "0";
+    //     } else {
+    //         document.getElementById("navbar").style.top = "-50px";
+    //     }
+    //     prevScrollpos = currentScrollPos;
+    // }
+
 });
--- dymm_app_web/views.py
+++ dymm_app_web/views.py
@@ -5,4 +5,14 @@ app_view = Blueprint('app_view', __name__, url_prefix='')
 
 @app_view.route('/')
 def index_view():
-    return render_template('home/base_home.html')
+    return render_template('/base.html')
+
+
+@app_view.route('/test')
+def test_view():
+    return render_template('test/base_test.html')
+
+
+@app_view.route('/test2')
+def test2_view():
+    return render_template('test/base_test2.html')
